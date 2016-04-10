1bda720166db97f007c84f5decf123a0db8e0253
mgp <info@mgp-it.de>
2016-04-10 15:14:46 +0200

    Test case refactoring. New test case containing the sample code from the javadoc on MQ.Configuration class.

--- src/test/de/mgpit/oracle/reports/plugin/commons/ConfigurationTest.java
+++ src/test/de/mgpit/oracle/reports/plugin/commons/ConfigurationTest.java
@@ -21,7 +21,7 @@ public class ConfigurationTest extends TestCase {
         URI testUri = null;
         boolean uriOK = true;
         try {
-            testUri = new URI( "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@QMGR?channelName=CHANNEL_1");
+            testUri = new URI( "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@QMGR?channelName=CHANNEL_1" );
         } catch ( URISyntaxException syntax ) {
             uriOK = false;
         }
@@ -34,7 +34,7 @@ public class ConfigurationTest extends TestCase {
         URI testUri = null;
         boolean uriOK = true;
         try {
-            testUri = new URI( "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@QMGR?channelName=CHANNEL_1");
+            testUri = new URI( "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@QMGR?channelName=CHANNEL_1" );
         } catch ( URISyntaxException syntax ) {
             uriOK = false;
         }
@@ -47,23 +47,57 @@ public class ConfigurationTest extends TestCase {
         URI testUri = null;
         boolean uriOK = true;
         try {
-            testUri = new URI( "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@QMGR?channelName=CHANNEL_1");
+            testUri = new URI( "wmq", null, "localhost", 1414, "/dest/queue/QMGR.IN.QUEUE@QMGR", "channelName=CHANNEL_1", null );
         } catch ( URISyntaxException syntax ) {
             uriOK = false;
         }
         assertTrue( uriOK );
-        
+
         Configuration c1 = Configuration.fromURI( testUri );
         Configuration c2 = Configuration.fromURI( testUri );
         Configuration c3 = new Configuration( "localhost", 1414, "QMGR", "CHANNEL_1", "QMGR.IN.QUEUE" );
-        
-        assertEquals( c1, c2 ); 
+
+        assertEquals( c1, c2 );
         assertEquals( c2, c1 ); // kommutativ
         assertEquals( c1, c1 ); // reflexiv
-        
+
         assertEquals( c2, c3 );
         assertEquals( c1, c3 ); // transitiv
     }
-    
+
+    public void testQueryParameterOverridesWmqQmgr() {
+        String expectedUriString = "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@QMGR?channelName=CHANNEL_1";
+        boolean uriOK = true;
+        URI creationUri = null;
+        try {
+            creationUri = new URI(
+                    "wmq://localhost:1414/dest/queue/QMGR.IN.QUEUE@NONSENSE?connectQueueManager=QMGR&channelName=CHANNEL_1" );
+        } catch ( URISyntaxException syntax ) {
+            uriOK = false;
+        }
+        assertTrue( uriOK );
+
+        Configuration testConfiguration = Configuration.fromURI( creationUri );
+        URI configurationsUri = testConfiguration.toURI();
+
+        assertEquals( expectedUriString, configurationsUri.toString() );
+    }
+
+    public void testJavadocExampleCode() throws URISyntaxException {
+        URI uri = new URI( "wmq://localhost/dest/queue/QUEUE.IN@QMGR?channelName=CHANNEL_1" );
+        Configuration c1 = Configuration.fromURI( uri );
+
+        URI another = new URI( "wmq", null, "localhost", 1414, "/dest/queue/QUEUE.IN@QMGR", "channelName=CHANNEL_1", null );
+        Configuration c2 = Configuration.fromURI( another );
+
+        URI third = new URI( "wmq", null, "localhost", 1414, "/dest/queue/QUEUE.IN@FOO",
+                "connectQueueManager=QMGR&channelName=CHANNEL_1", null );
+        Configuration c3 = Configuration.fromURI( third );
+        
+        System.out.println( "C1 and C2 are " + (c1.equals( c2 ) ? "equal" : "different") );
+        System.out.println( "C2 and C3 are " + (c2.equals( c3 ) ? "equal" : "different") );
+        System.out.println( "C1 and C3 are " + (c1.equals( c3 ) ? "equal" : "different") );
+
+    }
 
 }
