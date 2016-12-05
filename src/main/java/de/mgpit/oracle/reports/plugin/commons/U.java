8339c11180ba30890d7e835d7f0097c550551172
mgp <info@mgp-it.de>
2016-12-05 14:23:02 +0100

    String sha256(String str)

--- src/main/java/de/mgpit/oracle/reports/plugin/commons/U.java
+++ src/main/java/de/mgpit/oracle/reports/plugin/commons/U.java
@@ -20,13 +20,13 @@
 package de.mgpit.oracle.reports.plugin.commons;
 
 
+import java.security.MessageDigest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.StringUtils;
 
-import de.mgpit.oracle.reports.plugin.commons.io.IOUtility;
 import de.mgpit.types.Filename;
 import de.mgpit.types.TypedString;
 import oracle.reports.RWException;
@@ -107,6 +107,30 @@ public final class U {
 
     public static final String coalesce( String optionalString, String defaultString ) {
         return (optionalString == null) ? defaultString : optionalString;
+    }
+
+    /**
+     * Gets the SHA256 hash of the string given.
+     * 
+     * @param str String to hash
+     * @return SHA256 hash of the String given, hex encoded. In case of an exception during hashing {@code null} will be returned.
+     */
+    public static String sha256( String str ) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance( "SHA-256" );
+            byte[] hash = digest.digest( str.getBytes( "UTF-8" ) );
+            StringBuffer hexString = new StringBuffer();
+
+            for ( int i = 0; i < hash.length; i++ ) {
+                String hex = Integer.toHexString( 0xff & hash[i] );
+                if ( hex.length() == 1 ) hexString.append( '0' );
+                hexString.append( hex );
+            }
+
+            return hexString.toString();
+        } catch ( Exception ex ) {
+            return null;
+        }
     }
 
     /**
