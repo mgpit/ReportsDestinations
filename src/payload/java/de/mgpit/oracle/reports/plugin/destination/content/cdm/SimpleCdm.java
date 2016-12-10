132ec150a4198a50553bd03e967556612ee395a2
mgp <info@mgp-it.de>
2016-12-10 19:57:32 +0100

    Added license information.

--- src/payload/java/de/mgpit/oracle/reports/plugin/destination/content/cdm/SimpleCdm.java
+++ src/payload/java/de/mgpit/oracle/reports/plugin/destination/content/cdm/SimpleCdm.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2016 Marco Pauls www.mgp-it.de
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * @license APACHE-2.0
+ */
 package de.mgpit.oracle.reports.plugin.destination.content.cdm;
 
 
@@ -59,23 +78,16 @@ public class SimpleCdm extends AbstractCdm {
            .add( "bar" )
            .withData( "Lorem Ipsum" )
            .unnest()
-           .add( "doe" ).withData( "Dolor sit amet" )
+           .add( "doe" )
+           .withData( "Dolor sit amet" )
            .unnest()
            .add( "data" )
            .withData( "" );
 
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-
-        transformer.setOutputProperty( OutputKeys.INDENT, "yes" );
-
-        DOMSource xmlSource = new DOMSource( cdm.get() );
-        StringWriter outputTargetContent = new StringWriter();
-        Result outputTarget = new StreamResult( outputTargetContent );
-
-        transformer.transform( xmlSource, outputTarget );
-        System.out.println( outputTargetContent.toString() );
-        return outputTargetContent.toString();
+        // System.out.println( cdm.toString() );
+        // System.out.println( "\n" );
+        if ( hasAddress ) System.out.println( address.toString() );
+        return cdm.toString();
 
     }
 
